21f9c14ad204b36e844843619b5f6ba9ab31cb79
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2018-08-02 21:55:05 +0530

    sample cs file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GuiTurboAssembler
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Dummy C# file for github language detection");
+        }
+    }
+}
